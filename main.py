db6d0808b86a6a971da2d7b7fc511876383b2d78
DirtyLaundry <faisaljones@gmail.com>
2026-03-23 23:21:59 -0400

    Lists very many counterpoint valid 8 note sequences

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import random
 
 def new_series():
-    series = []
-    for i in range(0, 8):
-        series.append(random.randint(1, 7))
+    series = [0]
+    for i in range(1, 8):
+        while True:
+            next_pitch = random.randint(0, 7)
+            last_pitch = series[-1]
+            next_last_pitch = series[-2] if len(series) > 1 else None
+            leap_rule_down = next_last_pitch is not None and next_last_pitch - last_pitch > 1 and next_pitch - last_pitch != 1
+            leap_rule_up = next_last_pitch is not None and next_last_pitch - last_pitch < -1 and next_pitch - last_pitch != -1
+            if next_pitch == last_pitch or abs(next_pitch - last_pitch) > 4 or leap_rule_down or leap_rule_up:
+                continue                    
+            series.append(next_pitch)
+            break
     return series
 
 
 def make_series_set():
     series_set = set()
-    for i in range(0, 7**8):
-        series_set.add(tuple(new_series()))
+    attempts_without_new = 0
+    while True and attempts_without_new < 100000:
+        series = tuple(new_series())
+        if series not in series_set:
+            series_set.add(series)
+            attempts_without_new = 0
+        else:
+            attempts_without_new += 1
     series_list = list(series_set)
     return series_list
-    
 
-make_series_set()
-print(make_series_set())
+
+    
+print(len(make_series_set()))
