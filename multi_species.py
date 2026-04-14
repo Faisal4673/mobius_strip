48b6e9672a70e3ab956a941f88c2d007d80edd64
DirtyLaundry <faisaljones@gmail.com>
2026-04-14 08:04:08 -0400

    working multiseries reverser, messy code, functional programming

--- multi_species.py
+++ multi_species.py
@@ -1,79 +1,30 @@
 import random
-
+import itertools
 
 def get_prev_pitches(series, index, index2):
-    prev_pitches = []
 
-    #Last Beat exists:
-    last_beat_exists = index - 1 >= 0
-    #Second to Last Beat exists:
-    second_to_last_beat_exists = index - 2 >= 0
-    #Cases of 2 previous pitches:
-    # First note in beat, two notes in previous beat:
-    two_previous_pitches_case1 = index2 == 0 and len(series[index - 1]) == 2 and last_beat_exists
-    # Second note in beat, one note in previous beat:
-    two_previous_pitches_case2 = index2 == 1 and len(series[index - 1]) == 1 and last_beat_exists
-    # Second note in beat, two notes in previous beat:
-    two_previous_pitches_case3 = index2 == 1 and len(series[index - 1]) == 2 and last_beat_exists
-    # First note in beat, one note in previous 2 beats:
-    two_previous_pitches_case4 = index2 == 0 and len(series[index - 1]) == 1 and len(series[index - 2]) == 1 and last_beat_exists and second_to_last_beat_exists
-    # First note in beat, one note in previous beat, two notes in previous 2 beats:
-    two_previous_pitches_case5 = index2 == 0 and len(series[index - 1]) == 1 and len(series[index - 2]) == 2 and last_beat_exists and second_to_last_beat_exists
-
-    #Cases of 1 previous pitch:
-    # First note in beat, one note in previous beat:
-    one_previous_pitch_case1 = index2 == 0 and len(series[index - 1]) == 1 and last_beat_exists
-    # Second note in beat, first beat:
-    one_previous_pitch_case2 = index == 0 and len(series[index]) == 2 and index2 == 1
+    flat = [pitch for beat in series[:index] for pitch in beat] + series[index][:index2]
     
-    if two_previous_pitches_case1:
-        prev_pitches.append(series[index - 1][0])
-        prev_pitches.append(series[index - 1][1])
-    elif two_previous_pitches_case2:
-        prev_pitches.append(series[index - 1][0])
-        prev_pitches.append(series[index][0])
-    elif two_previous_pitches_case3:
-        prev_pitches.append(series[index - 1][1])
-        prev_pitches.append(series[index][0])
-    elif two_previous_pitches_case4:
-        prev_pitches.append(series[index - 2][0])
-        prev_pitches.append(series[index - 1][0])
-    elif two_previous_pitches_case5:
-        prev_pitches.append(series[index - 2][1])
-        prev_pitches.append(series[index - 1][0])
-    elif one_previous_pitch_case1:
-        prev_pitches.append(series[index - 1][0])
-    elif one_previous_pitch_case2:
-        prev_pitches.append(series[index][0])
-    else:       
-        print("No previous pitches found.")
+    return flat[-2:]  # Return the last two pitches before the current one
 
-    return prev_pitches
-
-
-
-    
-
-    
-
-def new_multi_species_series():
+def new_multi_species_series(length, species):
 
     
     #length = int(input("Input Series Length: "))
     series = []
-    for i in range(8):
+    for i in range(length):
         series.append([])
 
 
     for beat in series:
-        num_pitches = random.randint(1, 2)
+        num_pitches = random.randint(1, species)
         if num_pitches == 1:
             beat.append(random.randint(0, 7))
         else:
             beat.append(random.randint(0, 7))
             beat.append(random.randint(0, 7))
 
-    print("Generated Series: ", series)
+    #print("Generated Series: ", series)
     return series
 
 
@@ -110,19 +61,87 @@ def fix_series(series):
         for index2, pitch in enumerate(beat):
             prev_pitches = get_prev_pitches(series, index, index2)
             while not is_valid_motion(pitch, prev_pitches):
-                print(f"Invalid motion found at beat {index}, pitch {pitch}. Previous pitches: {prev_pitches}")
                 new_pitch = random.randint(0, 7)
-                print(f"Trying new pitch: {new_pitch}")
                 if is_valid_motion(new_pitch, prev_pitches):
-                    print(f"New pitch {new_pitch} is valid. Updating series.")
                     series[index][index2] = new_pitch
                     break
-                else:
-                    print(f"New pitch {new_pitch} is still invalid. Retrying.")
-    print(series)
+    #print("Fixed series: ", series)
     return series
 
-fix_series(new_multi_species_series())
+
+
+def is_reversable(series):
+    reverse_series = [beat[::-1] for beat in series[::-1]]
+    counter = 0
+    consonant_intervals = [2, 4, 5, 7]
+    print("Series: ", series)
+    print("Reverse series: ", reverse_series)
+
+
+    for index, (beat, reverse_beat) in enumerate(zip(series, reverse_series)):
+        counter2 = 0
+        if len(beat) > len(reverse_beat):
+            for index2, (pitch, reverse_pitch) in enumerate(zip(beat, itertools.cycle(reverse_beat))):
+                print("pitch: ", pitch, "reverse pitch: ", reverse_pitch)
+                if abs(pitch - reverse_pitch) in consonant_intervals:
+                    counter2 += 1
+                if abs(pitch - reverse_pitch) not in consonant_intervals and counter2 == 1 and get_prev_pitches(series, index, index2)[-1:] and abs(get_prev_pitches(series, index, index2)[-1:][0] - pitch) == 1:
+                    counter2+= 1
+            if counter2 == len(beat):
+                counter += 1
+        elif len(beat) < len(reverse_beat):
+            for index2, (pitch, reverse_pitch) in enumerate(zip(itertools.cycle(beat), reverse_beat)):
+                if abs(pitch - reverse_pitch) in consonant_intervals:
+                    counter2 += 1
+                if abs(pitch - reverse_pitch) not in consonant_intervals and counter2 == 1 and get_prev_pitches(reverse_series, index, index2)[-1:] and abs(get_prev_pitches(reverse_series, index, index2)[-1:][0] - pitch) == 1:
+                    counter2+= 1
+            if counter2 == len(beat):
+                counter += 1
+        else: 
+            for pitch, reverse_pitch in zip(beat, reverse_beat):
+                if abs(pitch - reverse_pitch) in consonant_intervals:
+                    counter2 += 1
+            if counter2 == len(beat):
+                counter +=1
+    
+
+
+    if counter == len(series):
+        return True
+    else: return False
+
+def make_reversable_series():
+
+    reversables_found = 0
+    while reversables_found == 0:
+        series = new_multi_species_series(8, 2)
+        if is_reversable(fix_series(series)):
+            reversables_found += 1
+    print("found reversable series!: ", series)
+    return series
+                             
+
+def convert_to_notes(series):
+    note_dict = { 
+        0: "C",
+        1: "D",
+        2: "E",
+        3: "F",
+        4: "G",
+        5: "A",
+        6: "B",
+        7: "C"
+    }
+    converted_series = [[note_dict[pitch] for pitch in beat] for beat in series]
+    print(converted_series)
+
+    
+
+#$check_reversable(fix_series(new_multi_species_series()))
+
+convert_to_notes(make_reversable_series())
+
+#is_reversable(fix_series(new_multi_species_series()))
 
     
     
@@ -144,6 +163,5 @@ fix_series(new_multi_species_series())
 
 #new_multi_species_series()
 
-#return_prev_pitches(new_multi_species_series())
 
 #print(type(get_prev_pitches(new_multi_species_series(), 0, 0)))
